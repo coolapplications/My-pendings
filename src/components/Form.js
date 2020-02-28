9a2f641c75e1c33c02251b34e1cdbd3db17acade
Luis Giraldo <luis.giraldo10@udea.edu.co>
2020-02-27 23:33:59 -0500

    Feat: form fields updated with database fields and data order

--- src/components/Form.js
+++ src/components/Form.js
@@ -21,7 +21,20 @@ function Form(props) {
   return (
     <div>
       <div className='container'>
+        <div className='form-group'>
+          <label forhtml='title'>Title</label>
+          <input
+            type='text'
+            className='form-control'
+            id='title'
+            placeholder='Your todo title'
+          />
+        </div>
         <form onSubmit={handleSubmit}>
+          <div className='form-group'>
+            <label forhtml='text'>Write your pending</label>
+            <textarea className='form-control' id='text' rows='3'></textarea>
+          </div>
           <div className='form-group'>
             <label forhtml='responsible'>Responsible</label>
             <input
@@ -32,8 +45,19 @@ function Form(props) {
             />
           </div>
           <div className='form-group'>
-            <label forhtml='text'>Write your pending</label>
-            <textarea className='form-control' id='text' rows='3'></textarea>
+            <label forhtml='responsible'>Responsible</label>
+            <input
+              type='text'
+              className='form-control'
+              id='responsible'
+              placeholder='Your name'
+            />
+          </div>
+          <div class='form-check'>
+            <input type='checkbox' class='form-check-input' id='isCompleted' />
+            <label class='form-check-label' for='isCompleted'>
+              Is completed?
+            </label>
           </div>
           <button type='submit' className='btn btn-primary'>
             Add Pending stuff
